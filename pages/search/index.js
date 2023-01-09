bd976dcd14dc864f7e17d3cd6ba6b87e31b1e7bc
ArfHere <54830824+arifals27@users.noreply.github.com>
2023-01-09 11:32:07 +0700

    Add files via upload

---
+++ pages/search/index.js
@@ -0,0 +1,47 @@
+import React, {Component, useState} from "react";
+import Card from "../../components/Cards";
+import Pagination from "../../components/Pagination";
+import Head from "next/head";
+
+const getData = async (query) => {
+    const url = `${process.env.API_URL}/?s=${query}`
+    const response = await fetch(url)
+    return response.json()
+}
+const Search = ({data}) => {
+    return(
+        <>
+            <Head>
+                <title>{`Search Result: "${data.query}" - ${data.site_title}`}</title>
+            </Head>
+            <div className="w-auto pt-5">
+                <div className="container my-6 mx-auto px-4 md:px-12">
+                    <div className="flex">
+                        <h2 className="text-bold text-3xl pb-3 border-b-4 border-solid">Search Result: {data.query}
+                        </h2>
+                    </div>
+                    <div className="flex flex-wrap -mx-1 lg:-mx-4 align-center justify-center">
+                        {
+                            data.itemLists.map((content, i) => {
+                                return <Card key={content.id} content={content}/>
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+            <Pagination max={data.oldest_page} now={data.current_page} slug={`/search/${data.query}`} />
+        </>
+    )
+}
+
+// This gets called on every request
+export async function getServerSideProps(context) {
+    context.res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=60, stale-while-revalidate=600'
+    )
+    const myData = await getData(context.query.s)
+    return { props: {data : myData} }
+}
+
+export default Search
